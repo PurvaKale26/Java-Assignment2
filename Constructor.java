0567290056a1a87056dcf55ec8ad38da55ffd775
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-08-11 10:56:22 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,18 @@
+public class Constructor{
+String name;
+int id;
+public  Constructor(){
+name="Purva";
+id=113;
+}
+
+public static void main(String args[]){
+ Constructor c=new Constructor();
+ System.out.println("Employee information");
+ System.out.println("Employee name: "+c.name);
+ System.out.println("Employee id: "+c.id);
+}
+}
+
+
+ 
