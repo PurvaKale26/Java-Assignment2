7139c3ac15e6f73430566cb8dc47e9934c7de306
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-08-11 10:54:15 +0530

    Add files via upload

---
+++ Thiskeyword2.java
@@ -0,0 +1,14 @@
+public class Thiskeyword2{
+public void display(){
+this.show();
+System.out.println("Display");
+}
+public void show(){
+
+System.out.println("Show");
+}
+public static void main(String args[]){
+Thiskeyword2 obj=new Thiskeyword2();
+obj.display();
+}
+}
